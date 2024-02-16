efa452e33e6c00275f75fabc6ba4d0f55188d0e5
Jorge Azuara <jorgeluisah47@gmail.com>
2024-02-16 00:13:01 -0600

    App.jsx -> Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import '../assets/styles/Home.css'
+
+function Home() {
+  return (
+    <div>
+      <h2>Hmmm... There should be something here...</h2>
+      <h3>I'm gonna work on it</h3>
+
+      <p><i>- robtzcraft</i></p>
+    </div>
+  )
+}
+
+export default Home
