27f83329ec2d6ddfd5021a96264780933877285a
Jorge Azuara <jorgeluisah47@gmail.com>
2024-02-15 23:57:39 -0600

    Directory structure set

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# robtzcraft_website
+<h3 style="color:blue"><strong><i>robtzcraft website</i></strong></h3>
---
+++ src/assets/pages/App.jsx
@@ -0,0 +1,14 @@
+import '../styles/App.css'
+
+function App() {
+  return (
+    <div>
+      <h2>Hmmm... There should be something here...</h2>
+      <h3>I'm gonna work on it</h3>
+
+      <p><i>- robtzcraft</i></p>
+    </div>
+  )
+}
+
+export default App
