e0849767e7d36ea890be242c63ba4a1db3f7ee22
Jorge Azuara <jorgeluisah47@gmail.com>
2024-02-16 00:11:35 -0600

    App.jsx -> Home.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './assets/pages/App.jsx'
+import Home from './pages/Home.jsx'
 import './assets/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
